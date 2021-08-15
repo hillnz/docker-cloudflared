a86377d9e78def7682e21e8302ed9348ff898cc2
Jono Hill <jono@hillnz.com>
2021-08-15 18:06:04 +1200

    Add custom entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Support old behaviour, i.e. before home was /config
+if [ -n "$(ls -A /home/nonroot)" ] && [ -z "$(ls -A /config)" ]; then
+    echo "Using configuration in /home/nonroot is deprecated. Mount your configuration at /config."
+    ln -s /home/nonroot /config
+fi
+
+if [ -z "$TUNNEL_CRED_FILE" ]; then
+    TUNNEL_CRED_FILE="/config/.cloudflared/${TUNNEL_ID}.json"
+fi
+
+make_config() {
+    if [ -f "$CONF_FILE" ]; then
+        return
+    fi
+    mkdir -p "$(dirname "$CONF_FILE")"
+    # shellcheck disable=SC2016
+    echo '{}' | jq \
+        --arg ACCOUNT_ID "$ACCOUNT_ID" \
+        --arg TUNNEL_ID "$TUNNEL_ID" \
+        --arg TUNNEL_NAME "$TUNNEL_NAME" \
+        --arg TUNNEL_SECRET "$TUNNEL_SECRET" \
+        '.AccountTag = $ACCOUNT_ID | .TunnelID = $TUNNEL_ID | .TunnelName = $TUNNEL_NAME | .TunnelSecret = $TUNNEL_SECRET' \
+        >"$CONF_FILE"
+}
+
+args=()
+if [ "$1" = "" ]; then
+    if [ -n "$ACCOUNT_ID" ] && [ -n "$TUNNEL_ID" ] && [ -n "$TUNNEL_NAME" ] && [ -n "$TUNNEL_SECRET" ]; then
+        make_config
+        args=(tunnel run "$TUNNEL_ID")
+    else
+        args+=(version)
+    fi
+else
+    args=("$@")
+fi
+
+exec cloudflared --no-autoupdate "${args[@]}"
